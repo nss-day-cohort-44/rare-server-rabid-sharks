51bf022ba65eea768bb7aeebf66d86ddaf958dec
negative1skunk@gmail.com <negative1skunk@gmail.com>
2021-01-27 20:18:20 -0600

    finished needed initial cru functions

--- models/user.py
+++ models/user.py
@@ -1,10 +1,11 @@
 class USER():
 
-    def __init__(self, id, first_name, last_name, email, bio, username, profile_image_url, active, account_type_id):
+    def __init__(self, id, first_name, last_name, email, password, bio, username, profile_image_url, active, account_type_id):
         self.id = id
         self.first_name = first_name
         self.last_name = last_name
         self.email = email 
+        self.password = password
         self.bio = bio
         self.username = username
         self.profile_image_url = profile_image_url
--- users/request.py
+++ users/request.py
@@ -16,12 +16,14 @@ def get_all_users():
             u.first_name,
             u.last_name,
             u.email,
+            u.password,
             u.bio,
             u.username,
             u.profile_image_url,
             u.created_on,
             u.active,
             account_type_id
+        WHERE u.id = ?
         FROM User u
         """)
 
@@ -31,13 +33,13 @@ def get_all_users():
 
         for row in dataset:
 
-            user = USER(row["id"],row["first_name"],row["last_name"],row["email"],row["bio"],row["username"],row["profile_image_url"],row["created_on"],row["active"],row["account_type_id"])
+            user = USER(row["id"], row["first_name"], row["last_name"], row["email"], row["password"], row["bio"], row["username"], row["profile_image_url"], row["created_on"], row["active"], row["account_type_id"])
 
             users.append(user.__dict__)
         
     return json.dumps(users)
 
-def get_single_users(id):
+def get_single_user(id):
     # Open a connection to the database
     with sqlite3.connect("./rare.db") as conn:
         # Just use these. It's a Black Box.
@@ -50,6 +52,7 @@ def get_single_users(id):
             u.first_name,
             u.last_name,
             u.email,
+            u.password,
             u.bio,
             u.username,
             u.profile_image_url,
@@ -62,8 +65,56 @@ def get_single_users(id):
         data= db_cursor.fetchone()
 
 
-        user = USER(data["id"],data["first_name"],data["last_name"],data["email"],data["bio"],data["username"],data["profile_image_url"],data["created_on"],data["active"],data["account_type_id"])
+        user = USER(data["id"],data["first_name"],data["last_name"],data["email"],data["password"],data["bio"],data["username"],data["profile_image_url"],data["created_on"],data["active"],data["account_type_id"])
 
-            users.append(user.__dict__)
         
-    return json.dumps(users)    
+        
+    return json.dumps(user.__dict__)    
+
+def create_user(new_user):
+    with sqlite3.connect("./rare.db") as conn:
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        INSERT INTO USER
+            ( first_name, last_name, email, password, bio, username, created_on, profile_image_url, active, account_type_id )
+        VALUES
+            ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+        """, (new_user['first_name'], new_user['last_name'],
+              new_user['email'], new_user['password'] , new_user['bio'],
+              new_user['username'], new_user["created_on"], new_user['profile_image_url'],new_user['active'],new_user['account_type_id'],))
+
+        id = db_cursor.lastrowid
+
+    return json.dumps(new_user)
+
+def update_user(id, new_user):
+    # Open a connection to the database
+    with sqlite3.connect("./rare.db") as conn:
+        # Just use these. It's a Black Box.
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        UPDATE USER
+            SET
+                u.first_name = ?
+                u.last_name = ?
+                u.email = ?
+                u.password = ?
+                u.bio = ?
+                u.username = ?
+                u.profile_image_url = ?
+                u.created_on = ?
+                u.active = ?
+                account_type_id = ?
+        WHERE id = ?
+        """, (new_user["first_name"],new_user["last_name"],new_user["email"],new_user["password"],new_user["bio"],new_user["username"],new_user["profile_image_url"],new_user["created_on"],new_user["active"],new_user["account_type_id"], id),)
+
+        rows_affected = db_cursor.rowcount
+    if rows_affected == 0:
+        return False
+    else:
+        return True
+
+
