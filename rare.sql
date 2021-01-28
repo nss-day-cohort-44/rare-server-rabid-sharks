15e9d64b90d81da6eb609984110a74965b5a63ef
Danny McCracken <dannymccracken13@yahoo.com>
2021-01-28 12:03:33 -0600

    sql updates

--- rare.sql
+++ rare.sql
@@ -7,6 +7,7 @@ CREATE TABLE "Users" (
   "first_name" varchar,
   "last_name" varchar,
   "email" varchar,
+  "password" varchar,
   "bio" varchar,
   "username" varchar,
   "profile_image_url" varchar,
@@ -46,6 +47,7 @@ CREATE TABLE "Comments" (
   "post_id" INTEGER,
   "author_id" INTEGER,
   "content" varchar,
+  "created_on" DATETIME,
   FOREIGN KEY(`post_id`) REFERENCES `Posts`(`id`),
   FOREIGN KEY(`author_id`) REFERENCES `Users`(`id`)
 );
@@ -82,3 +84,18 @@ CREATE TABLE "Categories" (
 
 INSERT INTO `Posts` VALUES (null, 1, 2, "Title 1", 01282021, "fake url", "fake news", TRUE);
 INSERT INTO `Posts` VALUES (null, 1, 2, "Title 2", 01282021, "fake url", "fake news", TRUE);
+
+INSERT INTO `Users` VALUES(null, "Silas", "Lowe", "silas@lowe.gmal", "Ahhhh", "I are me", "Slowe", "www.silaslowe.net", "10/10/10", "True", 2);
+INSERT INTO `Users` VALUES(null, "Frank", "Frankerson", "frank@lowe.gmal", "Yaaaaaaa", "I are not me", "Flowe", "www.frankerson.net", "11/11/11", "True", 1);
+
+INSERT INTO `Tags` VALUES (null, "Food");
+INSERT INTO `Tags` VALUES (null, "Mouth-breathing");
+INSERT INTO `Tags` VALUES (null, "Sports");
+
+INSERT INTO `Comments` VALUES (null, 1, 2, "Comment 1", 01282021);
+INSERT INTO `Comments` VALUES (null, 2, 1, "Comment 2", 01292021);
+INSERT INTO `Comments` VALUES (null, 1, 2, "Comment 3", 01302021);
+INSERT INTO `Comments` VALUES (null, 2, 1, "Comment 4", 01302021);
+
+INSERT INTO `Categories` VALUES ( null, "Category 1");
+INSERT INTO `Categories` VALUES ( null, "Category 2");
