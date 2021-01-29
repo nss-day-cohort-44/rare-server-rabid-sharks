df979f9ddf71b275e89f3c934fb9ab13d7290b4d
KyleSimmonsC44 <darrensimmons92@gmail.com>
2021-01-29 09:24:25 -0600

    Added join tables to my posts to show pertinent data needed to complete issue tickets

--- posts/request.py
+++ posts/request.py
@@ -67,15 +67,23 @@ def get_single_post(id):
             p.publication_date,
             p.image_url,
             p.content,
-            p.approved
+            p.approved,
+            c.label label
         FROM posts p
+        JOIN Categories c
+            ON p.category_id = c.id
         WHERE p.id = ?
         """, (id, ))
 
 
         data = db_cursor.fetchone()
 
+        category = Category(data['id'], data['label'])
+
+
         post = Post(data['id'], data['user_id'], data['category_id'], data['title'], data['publication_date'], data['image_url'], data['content'], data['approved'])
+
+        post.category = category.__dict__
 
     return json.dumps(post.__dict__)
 
@@ -143,9 +151,12 @@ def get_posts_by_user_id(user_id):
             p.publication_date,
             p.image_url,
             p.content,
-            p.approved
-        from posts p
-        where p.user_id = ?
+            p.approved,
+            c.label label
+        FROM posts p
+        JOIN Categories c
+            ON p.category_id = c.id
+        WHERE p.user_id = ?
         """, ( user_id, ))
 
         posts = []
@@ -153,5 +164,11 @@ def get_posts_by_user_id(user_id):
 
         for row in dataset:
             post = Post(row['id'], row['user_id'], row['category_id'], row['title'], row['publication_date'], row['image_url'], row['content'], row['approved'])
+
+            category = Category(row['id'], row['label'])
+
+            post.category = category.__dict__
+
             posts.append(post.__dict__)
+
     return json.dumps(posts)
