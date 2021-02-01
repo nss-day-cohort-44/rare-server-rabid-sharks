cd7a113040dc8818cdb565959b31cb6de0e71c72
KyleSimmonsC44 <darrensimmons92@gmail.com>
2021-02-01 09:37:15 -0600

    checkout to integrate new main

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ from .tag import Tag
 from .comment import Comment
 from .user import USER
 from .category import Category
+from .post_tag import Post_tag
---
+++ models/post_tag.py
@@ -0,0 +1,5 @@
+class Post_tag():
+    def __init__(self, id, label):
+        self.id = id
+        self.label = label
+        
