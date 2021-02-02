db0511baab7c66597b265b99fab4181788488499
Danny McCracken <dannymccracken13@yahoo.com>
2021-02-02 14:37:38 -0600

    fixed sql query to display categories in alphabetical order

--- categories/request.py
+++ categories/request.py
@@ -40,6 +40,7 @@ def get_all_categories():
             c.id,
             c.label
         FROM Categories c
+        ORDER BY label ASC;
         """)
 
         # Initialize an empty list to hold all category representations
